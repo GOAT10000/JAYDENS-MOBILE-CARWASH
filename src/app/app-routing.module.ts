07afc33040105f5e75f4a03d34f2401b5a4805c6
MalvrickBrown <143737567+MalvrickBrown@users.noreply.github.com>
2024-06-20 10:57:12 -0500

    pushing front end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { OurServicesComponent } from './pages/our-services/our-services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', title: 'Home Page', component:HomeComponent},
+  {path: 'Our-Service', title: 'Our Service', component:OurServicesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pages/partials/header/header.component';
+import { FooterComponent } from './pages/partials/footer/footer.component';
+import { OurServicesComponent } from './pages/our-services/our-services.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    OurServicesComponent,
   ],
   imports: [
     BrowserModule,
